dd1d66a3baf80161519f0179fc4fcf3634e2b3e6
azamat <azamatabdurashitov0@gmail.com>
2023-10-19 00:34:57 +0500

    create models

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path
-]
+]    
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
